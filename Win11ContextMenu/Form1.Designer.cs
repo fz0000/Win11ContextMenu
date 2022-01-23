1c95fba17b898efbff79c0c7a6e8df28f62c2bd0
fangzheng <fz0000@sina.com>
2022-01-23 23:34:57 +0800

    添加项目文件。

---
+++ Win11ContextMenu/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Win11ContextMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonClassic = new System.Windows.Forms.Button();
+            this.buttonDefault = new System.Windows.Forms.Button();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonClassic
+            // 
+            this.buttonClassic.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonClassic.Location = new System.Drawing.Point(264, 18);
+            this.buttonClassic.Name = "buttonClassic";
+            this.buttonClassic.Size = new System.Drawing.Size(150, 50);
+            this.buttonClassic.TabIndex = 1;
+            this.buttonClassic.Text = "&Classic";
+            this.buttonClassic.UseVisualStyleBackColor = true;
+            this.buttonClassic.Click += new System.EventHandler(this.buttonClassic_Click);
+            // 
+            // buttonDefault
+            // 
+            this.buttonDefault.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonDefault.Location = new System.Drawing.Point(450, 18);
+            this.buttonDefault.Name = "buttonDefault";
+            this.buttonDefault.Size = new System.Drawing.Size(150, 50);
+            this.buttonDefault.TabIndex = 2;
+            this.buttonDefault.Text = "&Default";
+            this.buttonDefault.UseVisualStyleBackColor = true;
+            this.buttonDefault.Click += new System.EventHandler(this.buttonDefault_Click);
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.SetColumnSpan(this.buttonRestart, 2);
+            this.buttonRestart.Location = new System.Drawing.Point(307, 106);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(250, 50);
+            this.buttonRestart.TabIndex = 3;
+            this.buttonRestart.Text = "&Restart Explorer";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonRestart, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonDefault, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonClassic, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonExit, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(618, 264);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. Set Menu Style:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. Restart Explorer:";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.SetColumnSpan(this.buttonExit, 2);
+            this.buttonExit.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonExit.Location = new System.Drawing.Point(357, 194);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(150, 50);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "E&xit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonExit;
+            this.ClientSize = new System.Drawing.Size(618, 264);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Windows 11 Context Menu";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClassic;
+        private System.Windows.Forms.Button buttonDefault;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Win11ContextMenu/Form1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Win11ContextMenu
+{
+    public partial class Form1 : Form
+    {
+        const string CLASSIC_REG = @"SOFTWARE\CLASSES\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonClassic_Click(object sender, EventArgs e)
+        {
+            Registry.CurrentUser.CreateSubKey(CLASSIC_REG + @"\InprocServer32");
+            MessageBox.Show("OK");
+        }
+        private void buttonDefault_Click(object sender, EventArgs e)
+        {
+            if (Registry.CurrentUser.OpenSubKey(CLASSIC_REG) != null)
+                Registry.CurrentUser.DeleteSubKeyTree(CLASSIC_REG);
+            MessageBox.Show("OK");
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            Process[] ps_list = Process.GetProcessesByName("explorer");
+            foreach (Process ps_explorer in ps_list)
+            {
+                ps_explorer.Kill();
+                ps_explorer.WaitForExit(1000);
+            }
+            ps_list = Process.GetProcessesByName("explorer");
+            for (int i = 0; i < 3; i++)
+            {
+                if (ps_list.Length > 0)
+                    break;
+                Thread.Sleep(200);
+            }
+            if (ps_list.Length == 0)
+            {
+                Process.Start("explorer.exe");
+            }
+        }
+    }
+}
